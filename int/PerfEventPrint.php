09aec88fe9064c97be4b76f4e9fcb3ad91309128
waveney <richard@wavwebs.com>
2023-05-23 13:07:03 +0100

    Print Changes

--- int/PerfEventPrint.php
+++ int/PerfEventPrint.php
@@ -2,8 +2,11 @@
   include_once("fest.php");
   include_once("ChangeLib.php");
 
-  dominimalhead("Event changes since the programme went to print",[],1);
+  dominimalhead("Event changes since the programme went to print",["files/Newheader.php","festcon.php",'files/Newstyle.css','css/PrintPage.css'],1);
+  echo "<div class=PaperL>";
+  echo "<h1>Event Changes since the programme went to print</h1>";
   EventChangePrint(2);
+  echo "<h1>Performer Changes since the programme went to print</h1>";
   PerfChangePrint(2);
   exit;
 ?>
