3ba3a52b7e6ed4d42a20c3e269e72eedc43d659f
waveney <richard@wavwebs.com>
2023-12-20 18:17:04 +0000

    Miss off unwanted action buttons 2

--- int/TradeFAQ.php
+++ int/TradeFAQ.php
@@ -2,7 +2,10 @@
   include_once("fest.php");
   dohead("Trade FAQ",[],1);
   
-  echo TnC("TradeTnC");
+    echo TnC('TradeTnC');
+    echo TnC('TradeTimes');
+    echo TnC('TradeFAQ');
+
   dotail();
 ?>
 
