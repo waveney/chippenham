09e42f291f0d8ffe4f6cb9256721729c7b0327c2
waveney <richard@wavwebs.com>
2024-02-28 12:37:47 +0000

    Better Maps 2

--- int/TradeStandMap.php
+++ int/TradeStandMap.php
@@ -6,21 +6,56 @@
   include_once("TradeLib.php");
 /* If logged in or trade stae >=partial view actual traders, otherwise just the grid */
 
-  global $Pitches,$tloc,$loc,$YEARDATA,$EType_States,$Traders;
+  global $Pitches,$tloc,$loc,$YEARDATA,$EType_States,$Traders,$USER,$USERID;
+  $TradeStates = array_flip($EType_States);
+  $TradeState = $YEARDATA['TradeState'];
+  $TradeViews = [0,1,2,2,2];
+  $ViewTrade = $TradeViews[$TradeState];
+  
 
   $loc = $_REQUEST['l'] ?? Feature('TradeBaseMap');
   if (!is_numeric($loc)) Error_Page("No Hacking please");
-  $Traders = [];
-  if (Access('Staff') || $YEARDATA['TradeState']>= (array_flip($EType_States))['Partial']) $Traders = Get_Traders_For($loc, (Access('Staff')?1:0));
-  $Pitches = Get_Trade_Pitches($loc);  
 
   $tloc = Get_Trade_Loc($loc);
   
   if(!$tloc) Error_Page("Unknown Map");
   
-  if (Access('Staff')) echo "Any Trader in White has not PAID<p>";
+  $ShowTraders = 0;
+  
+  $Type = ($REQUEST['t']??0); 
+  
+  switch ($Type) {
+    case 0: // Public, Setup, EMP, Infrastructure
+    default:
       
-  Pitch_Map($tloc,$Pitches,$Traders,1,1);
+      break;
+    
+    case 1: // Public with trade 
+    case 2: // Trader Only
+
+      if (Access('Staff') || ($ViewTrade == 2)) {
+        $ShowTraders = -1;
+      } elseif (($ViewTrade == 1 ) && Access('Participant','trade')) {
+        $ShowTraders = $USERID;
+      } 
+      break;
+    
+    case 4: // Assign
+      $ShowTraders = -1;
+      break;
+  }
+  
+  if ($ShowTraders < 0) {
+    $Traders = Get_Traders_For($loc, (Access('Staff')?1:0));
+  } else if($ShowTraders > 0) {
+    $Traders = [Get_Trader_All($ShowTraders)];
+  }
+    
+  $Pitches = Get_Trade_Pitches($loc);  
+
+  if (Access('Staff') && $Traders) echo "Any Trader in White has NOT PAID<p>";
+
+  Pitch_Map($tloc,$Pitches,$Traders,$Type,1);
   if ($loc != Feature('TradeBaseMap')) echo "<h2><a href=TradeStandMap>Return to main map</a></h2>";
 
   dotail();
