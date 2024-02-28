03f7ebbf37fe586d8bf9885f5e034f48dc795fa6
waveney <richard@wavwebs.com>
2024-02-28 16:24:08 +0000

    EMP map

--- int/Staff.php
+++ int/Staff.php
@@ -412,6 +412,7 @@
     
     $txt .= "<li><a href=TradeStandMap?t=6&Y=$YEAR>Infrastructure Map</a>\n";
     $txt .= "<li><a href=TradeStandMap?t=0&Y=$YEAR>Island Park General Map</a>\n";
+    $txt .= "<li><a href=TradeStandMap?t=5&Y=$YEAR>Island Park EMP Map</a>\n";
 
 
     $txt .= "</ul>\n";
