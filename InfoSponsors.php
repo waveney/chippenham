5bf503c342e5123ab339bb6fe0846595c068bc92
waveney <richard@wavwebs.com>
2024-03-21 14:39:27 +0000

    Sponsor glitch 4

--- InfoSponsors.php
+++ InfoSponsors.php
@@ -20,10 +20,10 @@
   foreach ($Spons as $s) {
     echo "<div class=sponflexcont>\n";
     if ($s['Website']) echo weblinksimple($s['Website']);
-    if ($s['Image']) echo "<img src='" . $s['Image'] . "' width=200>";
+    if (!empty($s['Photo']) || !empty($s['Logo'])) echo "<img src='" . (empty($s['Logo'])?$s['Photo']:$s['Logo']) . "' width=200>";
     echo "<div class=sponttl>" . $s['SN'] . "</div>";
     if ($s['Website']) echo "</a>";
-    if ($s['Description']) echo "<p>" . $s['Description'];
+    if ($s['GoodsDesc']) echo "<p>" . $s['GoodsDesc'];
     
     $Sponsrd = Gen_Get_Cond('Sponsorship',"Year=$SHOWYEAR AND SponsorId=" . $s['SponsorId'] . " ORDER BY Importance Desc");
     
