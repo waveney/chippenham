715a4f18e6011b4e073e2e4325a6d95e434b701c
Ben Robinson <4210111+bsrobinson@users.noreply.github.com>
2024-04-23 17:40:38 +0100

    Added zero priced (free) event prices

--- api/events.php
+++ api/events.php
@@ -110,6 +110,7 @@ function Get_Prices(&$Ev) {
     if ($Ev['SeasonTicketOnly']) {
         return [Make_Price($Cpri ? $Cpri : null, $Event_Access_Type[$Ev['SeasonTicketOnly']])];
     }
+    if (!$Cpri) return [Make_Price(0, null)];
   
     if ($Ev['TicketCode']) {
         $Ev['TicketUrl'] = $Ev['TicketCode'];
