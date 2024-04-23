761b1a452ec916428cb94ccd6f00e266c4dcbbd6
Ben Robinson <4210111+bsrobinson@users.noreply.github.com>
2024-04-23 14:02:22 +0100

    Format SlotEnd

--- api/events.php
+++ api/events.php
@@ -47,6 +47,9 @@ function Get_Events($subEventId) {
                 $row['Start'] = Get_Date($row['Day'], $row['Start']);
                 $row['End'] = Get_Date($row['Day'] + $row['EndsNextDay'], $row['End']);
             }
+            if ($row['SlotEnd'] > 0) {
+                $row['SlotEnd'] = Get_Date($row['Day'], $row['SlotEnd']);
+            }
             $row['Price'] = Price_Show($row);
             $row['SubEvents'] = Get_Events($row['Id']);
             $row["Access"] = $Event_Access_Type[$row['SeasonTicketOnly']];
