fbe4b49357d4563e831d253c12567f31b76b34c4
waveney <richard@wavwebs.com>
2024-05-16 12:01:34 +0100

    Check Web Links 3

--- int/CheckWebLinks.php
+++ int/CheckWebLinks.php
@@ -6,9 +6,10 @@ include_once("ProgLib.php");
 include_once("TradeLib.php");
 
 A_Check('Staff');
+global $StartAt, $BatchSize, $WCount;
 
 function CheckLink(&$Data,$Category,$Editor,$id) {
-  
+  global $CONF;
   $links = $Data['Website']??0;
   if (!$links) return;
   
@@ -16,51 +17,85 @@ function CheckLink(&$Data,$Category,$Editor,$id) {
   foreach($sites as $si=>$site) {
     $url = stripslashes($site);
     if (!preg_match("/^https?/i",$url,$mtch)) $url = 'http://' . $url;
-//    echo "Would check $Category - <a href=$Editor?id=$id>" . ($Data['SN']??$Data['Name']??'Unknown') . " </a>has a website - $site<p>";
-//    continue;
-    list($status) = get_headers($url);
-    if (strpos($status, '200') !== TRUE) {
-      echo "$Category - <a href=$Editor?id=$id>" . ($Data['SN']??$Data['Name']??'Unknown') . "</a> has an faulty website - $site<p>";
+    if ($CONF['testing']??0) {
+      echo "$Category - " . ($Data['SN']??$Data['Name']??'Unknown') . "<p>";
+      continue;
     }
-     
+// exit;
+    $headers = @ get_headers($url);
+    $Code = substr($headers[0], 9, 3);
+    if ($Code != '200') {
+      echo "$Category - <a href=$Editor?id=$id>" . ($Data['SN']??$Data['Name']??'Unknown') . "</a> has an faulty website - $site - failed $Code<p>";
+    } else {
+      echo "$Category - " . ($Data['SN']??$Data['Name']??'Unknown') . " website ok <br>";
+    }
   }
 }
 
-dostaffhead("Check Web Links");
+
+function CheckAll() {
+  global $StartAt, $BatchSize, $WCount, $Coming_Type,$YEAR,  $Book_State, $Trade_State,$db;
+
 // Check all Dancers performers in PLANYEAR
 
-$SideQ = $db->query("SELECT s.*, y.* " . 
-         "FROM Sides AS s, SideYear AS y WHERE s.SideId=y.SideId AND y.year='$YEAR' AND y.Coming=" . $Coming_Type['Y'] . 
-         " AND s.IsASide=1 AND s.NotPerformer=0 AND y.NoDanceEvents=0 ");
 
-if ($SideQ) while($side = $SideQ->fetch_assoc()) { 
-  CheckLink($side,'Dance Side','AddPerf',$side['SideId']);
+  $SideQ = $db->query("SELECT s.*, y.* " . 
+           "FROM Sides AS s, SideYear AS y WHERE s.SideId=y.SideId AND y.year='$YEAR' AND y.Coming=" . $Coming_Type['Y'] . 
+           " AND s.IsASide=1 AND s.NotPerformer=0 AND y.NoDanceEvents=0 AND Website!=''");
+
+  if ($SideQ) while($side = $SideQ->fetch_assoc()) { 
+//    echo "$WCount<p>";
+    if ($WCount >= $StartAt + $BatchSize) return 1;
+    if ($WCount++ < $StartAt) continue; 
+    CheckLink($side,'Dance Side','AddPerf',$side['SideId']);
+  }
+  echo "<br>Checked Dance Sides<p>";
+
+
+  // Check all Other performers in PLANYEAR
+
+  $SideQ = $db->query("SELECT s.*, y.*  FROM Sides AS s, SideYear AS y " .
+         "WHERE s.SideId=y.SideId AND y.year='$YEAR' AND y.YearState>=" . $Book_State['Booking'] . 
+         " AND s.NotPerformer=0 AND Website!=''");
+  if ($SideQ) while($side = $SideQ->fetch_assoc()) { 
+//    echo "$WCount<p>";
+    if ($WCount >= $StartAt + $BatchSize) return 2;
+    if ($WCount++ < $StartAt) continue; 
+    CheckLink($side,'Performer','AddPerf',$side['SideId']);
+  }
+  echo "<br>Checked All other performers<p>";
+
+  // Check all Traders in PLANYEAR
+
+  $TradeQ = $db->query("SELECT t.*, y.* FROM Trade AS t, TradeYear AS y WHERE t.Status!=2 AND t.Tid = y.Tid AND Website!='' AND y.Year='$YEAR'" .
+            " AND y.BookingState>" . $Trade_State['Submitted']);
+  if ($TradeQ) while($t = $TradeQ->fetch_assoc()) { 
+//    echo "$WCount<p>";    
+    if ($WCount >= $StartAt + $BatchSize) return 3;
+    if ($WCount++ < $StartAt) continue; 
+    CheckLink($t,'Trader','Trade',$t['Tid']);
+    }
+  echo "<br>Checked All Traders<p>";
+
+
+  // Check all Sponsors - TODO
+
+  // Check all Events - TODO
+  return 0;
 }
-echo "<br>Checked Dance Sides<p>";
 
+$StartAt = $_REQUEST['StartAt']??0;
+$BatchSize = 20;
+$WCount = 0;
 
-// Check all Other performers in PLANYEAR
+dostaffhead("Check Web Links");
+$R = CheckAll();
 
-$SideQ = $db->query("SELECT s.*, y.*  FROM Sides AS s, SideYear AS y " .
-       "WHERE s.SideId=y.SideId AND y.year='$YEAR' AND y.YearState>=" . $Book_State['Booking'] . 
-       " AND s.NotPerformer=0 ");
-if ($SideQ) while($side = $SideQ->fetch_assoc()) { 
-  CheckLink($side,'Performer','AddPerf',$side['SideId']);
+//if ($WCount >= $StartAt + $BatchSize) {
+if ($R) {
+  echo "<h2><a href=CheckWebLinks?StartAt=$WCount>Next $BatchSize links - $R</a></h2>";
+} else { 
+  echo "<h2>Finished</h2>";
 }
-echo "<br>Checked All other performers<p>";
-
-// Check all Traders in PLANYEAR
-
-$TradeQ = $db->query("SELECT t.*, y.* FROM Trade AS t, TradeYear AS y WHERE t.Status!=2 AND t.Tid = y.Tid AND y.Year='$YEAR' AND y.BookingState>" . 
-           $Trade_State['Submitted']);
-if ($TradeQ) while($t = $TradeQ->fetch_assoc()) { 
-  CheckLink($t,'Trader','Trade',$t['Tid']);
-}
-echo "<br>Checked All Traders<p>";
-
-
-// Check all Sponsors - TODO
-
-// Check all Events - TODO
 
 dotail();
